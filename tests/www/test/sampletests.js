6d595b09d5d67897a6b8a5d671f4e3eadc3af7ce
shrishrirang <shrishrirang@gmail.com>
2015-08-06 00:42:31 -0700

    Add cordova test project

---
+++ tests/www/test/sampletests.js
@@ -0,0 +1,17 @@
+describe("Calculator", function () {
+    beforeEach(function () {
+    });
+
+    describe("Test1", function () {
+        it("Dummy test 1", function () {
+            expect(21).to.be(1);
+        });
+    });
+
+    describe("Test2", function () {
+        it("Dummy test 2", function () {
+            expect(1).to.be(1);
+        });
+    });
+
+});
